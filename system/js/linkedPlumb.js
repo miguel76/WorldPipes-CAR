fd4f3bdaa487a5b6253ee4874e92441180487617
miguel76 <miguel.ceriani@gmail.com>
2014-06-24 18:12:13 +0200

    Introducing standard RDF serialization from jsPlumb

---
+++ system/js/linkedPlumb.js
@@ -0,0 +1,43 @@
+function linkedPlumb() {
+}
+
+linkedPlumb.jsPlumb2Turtle = function (componentElementList, jspInstance, graphWriter) {
+	
+	function writeTypes(typedObject, objectURI, graphWriter) {
+		typedObject.getType();
+		// TODO: write rdf:type statements from object (endpoint or connection) types
+	}
+	
+	function fromComponentElement(componentElement, graphWriter) {
+		// TODO: generate component URI
+		// TODO: write component data
+		var endpointList = jspInstance.getEndpoints(componentElement);
+		for (var epIndex = 0; epIndex < endpointList.length; epIndex++) {
+			fromEndpoint(endpointList[epIndex], graphWriter);
+		}
+		// TODO: return component URI
+	}
+	
+	function fromEndpoint(endpoint, graphWriter) {
+		// TODO: generate endpoint URI
+		// TODO: write endpoint data
+		// TODO: return endpoint URI
+	}
+	
+	function fromConnection(connection, graphWriter) {
+		// TODO: generate connection URI
+		// TODO: write connection data
+		// TODO: return connection URI
+	}
+	
+	// TODO: generate dataflow URI
+	// TODO: write dataflow data
+	for (var compIndex = 0; compIndex < componentElementList.length; compIndex++) {
+		fromComponentElement(componentElementList[compIndex], graphWriter);
+	}
+	var connectionList = jspInstance.getConnections();
+	for (var connIndex = 0; connIndex < connectionList.length; connIndex++) {
+		fromConnection(connectionList[connIndex], graphWriter);
+		// TODO: use returned component URI to connect to dataflow URI
+	}
+}
