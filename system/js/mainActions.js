fcb21320e0bca69f2e9b6ca5da33f3604969a8fc
miguel76 <miguel.ceriani@gmail.com>
2014-07-29 18:48:21 -0400

    Added code for asynchronous upload of a graph

--- system/js/mainActions.js
+++ system/js/mainActions.js
@@ -9,18 +9,37 @@ MainActions.newPipe = function() {
 
 MainActions.savePipe = function() {
 	
-	var sourcecode = Core.getElementsByClass("codeclass")[0];
-	Code.estraiTesto(
-			sourcecode, pulsante.title,
-//			GraphURIPrefix,
-			function(err) {
-				if (err) {
-					alert('Error: ' + JSON.stringify(err));
-					updateStatus('Error Saving Pipeline');
-				} else {
-					updateStatus('Pipeline Saved!');
-				}
+	var editor = Core.getElementsByClass("areaeditor")[0];
+//  Component.updatePositions(editor, componentVett);
+	
+	var store = N3.Store();
+	linkedPlumb.jsPlumbToRDF(
+//			document.getElementsByClassName("jsplumb-draggable"),
+			componentVett,
+			jsPlumb, store);
+	
+	var writer = N3.Writer();
+	writer.addTriples(store.find(null,null,null));
+	writer.end(
+			function (error, result) {
+				if (error)
+					console.error(error);
+				else
+					console.log("Store: " + result);
 			});
+	
+//	var sourcecode = Core.getElementsByClass("codeclass")[0];
+//	Code.estraiTesto(
+//			sourcecode, pulsante.title,
+////			GraphURIPrefix,
+//			function(err) {
+//				if (err) {
+//					alert('Error: ' + JSON.stringify(err));
+//					updateStatus('Error Saving Pipeline');
+//				} else {
+//					updateStatus('Pipeline Saved!');
+//				}
+//			});
 };
 MainActions.viewPipeProperties = function() {
 			//var json = JSON.stringify(componentVett);
@@ -41,8 +60,20 @@ MainActions.playPipe = function() {
 			});
 //	window.open("http://localhost:8080/swows-web/play?df=" + encodeURIComponent(URISystemGraphStore + encodeURIComponent(dataflowURI)),"_blank");
 };
+
 MainActions.reloadPipe = function() {
-	Component.jsonLoad();
+	
+	Component.refreshEditor(document.getElementById("areaeditor"));
+	
+	linkedPlumb.jsPlumbFromRDF(
+			store, null, jsPlumb,
+			{
+				generatorFor: function(objectType) {
+					return Component.factory.generatorFor(objectType) || Endpoint.factory.generatorFor(objectType);
+				}
+			});
+
+//	Component.jsonLoad();
 };
 MainActions.publishPipe = function() {
 	Code.sendCodeURIUpdate();
