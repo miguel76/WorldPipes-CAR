67df06133516910ddbea3cdeb6c71091b915ae5f
miguel76 <miguel.ceriani@gmail.com>
2014-07-31 17:35:38 -0400

    Added actions for pipeline save and reload

--- system/js/mainActions.js
+++ system/js/mainActions.js
@@ -1,46 +1,38 @@
 MainActions = {}
 
 MainActions.newPipe = function() {
-	if(componentVett.length != 0){
+	if(componentVett.length != 0) {
 		if(confirm("Sure you want to leave this page?")){location.reload();}
 	}
 	else{location.reload();}
 };
 
-MainActions.savePipe = function() {
+MainActions.savePipe = function(callback) {
+	
+	var outerCallback = callback;
+	var callback = function (err) {
+		if (err) {
+			alert('Error: ' + JSON.stringify(err));
+			updateStatus('Error Saving Pipeline');
+		} else {
+			updateStatus('Pipeline Saved!');
+		}
+		if (outerCallback)
+			outerCallback(err);
+	}
 	
 	var editor = Core.getElementsByClass("areaeditor")[0];
 //  Component.updatePositions(editor, componentVett);
 	
-	var store = N3.Store();
-	linkedPlumb.jsPlumbToRDF(
+	return linkedPlumb.jsPlumbToRDF(
 //			document.getElementsByClassName("jsplumb-draggable"),
 			componentVett,
-			jsPlumb, store);
-	
-	var writer = N3.Writer();
-	writer.addTriples(store.find(null,null,null));
-	writer.end(
-			function (error, result) {
-				if (error)
-					console.error(error);
-				else
-					console.log("Store: " + result);
-			});
-	
-//	var sourcecode = Core.getElementsByClass("codeclass")[0];
-//	Code.estraiTesto(
-//			sourcecode, pulsante.title,
-////			GraphURIPrefix,
-//			function(err) {
-//				if (err) {
-//					alert('Error: ' + JSON.stringify(err));
-//					updateStatus('Error Saving Pipeline');
-//				} else {
-//					updateStatus('Pipeline Saved!');
-//				}
-//			});
+			jsPlumb,
+			N3ServerSync.createCalliUpdateWriter(
+					resourceURI,
+					callback));
 };
+
 MainActions.viewPipeProperties = function() {
 			//var json = JSON.stringify(componentVett);
 };
@@ -49,28 +41,40 @@ MainActions.playPipe = function() {
 	Code.estraiTesto(
 			sourcecode, pulsante.title,
 //			GraphURIPrefix,
-			function(err) {
-				if (err) {
-					alert('Error: ' + JSON.stringify(err));
-					updateStatus('Error Saving Pipeline');
-				} else {
-					updateStatus('Pipeline Saved!');
-					window.open("http://localhost:8080/swows-web/play?df=" + encodeURIComponent(dataflowURI),"_blank");
-				}
-			});
 //	window.open("http://localhost:8080/swows-web/play?df=" + encodeURIComponent(URISystemGraphStore + encodeURIComponent(dataflowURI)),"_blank");
 };
 
-MainActions.reloadPipe = function() {
+MainActions.reloadPipe = function(callback) {
+	
+	var outerCallback = callback;
+	var callback = function (err) {
+		if (err) {
+			alert('Error: ' + JSON.stringify(err));
+			updateStatus('Error Loading Pipeline');
+		} else {
+			updateStatus('Pipeline Loaded!');
+		}
+		if (outerCallback)
+			outerCallback(err);
+	}
 	
 	Component.refreshEditor(document.getElementById("areaeditor"));
 	
-	linkedPlumb.jsPlumbFromRDF(
-			store, null, jsPlumb,
-			{
-				generatorFor: function(objectType) {
-					return Component.factory.generatorFor(objectType) || Endpoint.factory.generatorFor(objectType);
-				}
+	var store = N3.Store();
+	N3ServerSync.calliRead(
+			resourceURI,
+			store,
+			function(error) {
+				if (error)
+					callback(error);
+				linkedPlumb.jsPlumbFromRDF(
+						store, null, jsPlumb,
+						{
+							generatorFor: function(objectType) {
+								return Component.factory.generatorFor(objectType) || Endpoint.factory.generatorFor(objectType);
+							}
+						});
+				callback();
 			});
 
 //	Component.jsonLoad();
