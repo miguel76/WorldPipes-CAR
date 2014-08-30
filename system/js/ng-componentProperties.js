78f63cc7fc3c3c00cde311d4851cd085fdf7c80c
miguel <miguel.ceriani@gmail.com>
2014-08-30 13:47:38 -0400

    Adding input list mngmt capability to component properties form

--- system/js/ng-componentProperties.js
+++ system/js/ng-componentProperties.js
@@ -4,7 +4,22 @@
   app.controller('SelectedComponentPropertiesController', function() {
 	  this.openFor = function(component) {
 		  this.component = component;
+		  if (component.allowsMultipleInputs()) { 
+			  this.inputs = component.getInputEndpoints();
+			  this.inputsToBeDeleted = [];
+		  }
 	  };
+	  this.addInput = function() {
+		  this.inputs |= {
+				  shape: "dot",
+				  color: "red"
+		  };
+	  };
+	  this.deleteInput(input) = function() {
+		  this.inputs.remove(input);
+		  this.inputsToBeDeleted |= input.uuid;
+	  };
+	  this.uuid
 	  this.close = function() {
 		  this.component = null;
 	  };
